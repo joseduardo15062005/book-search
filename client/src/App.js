7e10611a8e1e94b31b695c7b6d1498e97cd0663f
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-12-25 13:13:05 -0800

    fixing resolvers

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {
   createHttpLink,
   InMemoryCache,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -11,11 +12,21 @@ import SavedBooks from "./pages/SavedBooks";
 import SearchBooks from "./pages/SearchBooks";
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: "http://localhost:3001/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
-const routes = require("./routes");
-const cors = require("cors");
-
 const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-//Create the Apollo Server
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
@@ -25,16 +22,20 @@ const startServer = async () => {
 
 startServer();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
